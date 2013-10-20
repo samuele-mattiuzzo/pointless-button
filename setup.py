bc98c35f3a88b041aee75a20f94aac7b82a63891
Samuele Mattiuzzo <samumatt@gmail.com>
2013-10-20 02:49:29 +0100

    Setup script

---
+++ setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/pyhon
+
+import os
+import sys
+from subprocess import call
+
+# new plugins need to go here
+PLUGINS = [
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git',  # Device API
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-network-information.git',  # Connection
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-battery-status.git',  # Battery API
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git',  # Geolocation
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-file.git',  # File API
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-file-transfer.git',  # Network transfer
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-dialogs.git',  # Dialog notifications
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-vibration.git',  # Vibration notifications
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git',  # Globalization
+]
+
+DEV_PLUGINS = [
+	'https://git-wip-us.apache.org/repos/asf/cordova-plugin-console.git',  # Debug console
+]
+
+REMOVE_PLUGINS = []
+
+# DO NOT CHANGE UNLESS PRE-DISCUSSED
+INSTALL_DIR = 'pointlessbutton/'
+
+def get_installed_plugins():
+	plugin_list = os.popen('cordova plugin ls')
+	print plugin_list
+	print typeof(plugin_list)
+	return []
+
+def clean():
+	for plugin in get_installed_plugins():
+		remove_plugin(plugin)
+
+def remove_plugin(plugin):
+	# helper for code readability
+	os.system('phonegap local plugin remove ' + plugin)
+
+def install_plugin(plugin):
+	# helper for code readability
+	os.system('phonegap local plugin add ' + plugin)
+
+os.chdir(INSTALL_DIR)
+user_args = sys.argv
+
+print os.getcwd()
+
+if len(user_args) > 2:
+	# No cheating
+	print 'Max one option at a time'
+
+else:
+	if '--help' in user_args:
+		# Displays a basic help message
+		print '''
+Helper utility:\n\n
+$ python app_plugins_install.py [options]
+  --help: displays this screen
+  --build_install: installs plugins needed for a release build
+  --dev_install: installs all plugins, dev included
+  --clean: removes all installed plugins
+  --remove: removes all plugins included in REMOVE_PLUGINS (do not use!)
+'''
+
+	elif '--build_install' in user_args:
+		# Launch this when you need a release build
+		clean()
+		for plugin in PLUGINS:
+			install_plugin(plugin)
+
+	elif '--dev_install' in user_args:
+		# Launch this while developing
+		clean()
+		for plugin in PLUGINS + DEV_PLUGINS:
+			install_plugin(plugin)
+
+	elif '--clean' in user_args:
+		# Cleans all packages from your disk
+		clean()
+		print 'Not Yet Implemented'
+
+	elif '--remove' in user_args:
+		# Removes packages selectively
+		for plugin in get_installed_plugins():
+			remove = raw_input('Remove {}? [y/n]'.format(plugin))
+			if remove in ['y', 'Y']:
+				remove_plugin(plugin)
+
+	else:
+		# 
+		print 'Invalid option'
